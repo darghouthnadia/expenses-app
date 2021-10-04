813d41c177c9d7c91cd7f86dda090677f79e8d3a
Darghouth Nadia <darghouthnadia@gmail.com>
2021-10-04 22:07:30 +0200

    Removed unnecessary comment

--- src/Components/Expenses/Expenses.js
+++ src/Components/Expenses/Expenses.js
@@ -16,23 +16,6 @@ const Expenses = ({items}) => {
         date={date}
       />
       )}
-      
-      
-            {/* <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      />
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      />
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      /> */}
     </Card>
   );
 }
